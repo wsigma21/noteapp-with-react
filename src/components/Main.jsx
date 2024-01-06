5aceb0ab23154c1fc218fa6f246fb5c039af160b
wsigma21 <wallstlbl@gmail.com>
2024-01-06 11:53:22 +0900

    add: mainコンポーネント作成・CSS

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
 import "./Main.css"
 
 function Main() {
   return (
-    <div>Main</div>
+    <div className="app-main">
+      <div className="app-main-note-edit">
+        <input type="text" />
+        <textarea id="" placeholder="ノート内容を記入"></textarea>
+      </div>
+      <div className="app-main-note-preview">
+        <h1 className="preview-title">タイトル</h1>
+        <div className="markdown-preview">ノート内容</div>
+      </div>
+    </div>
   )
 }
 
