6cc2729512c13b6a5b7001c6e28724c3bcf40755
wsigma21 <wallstlbl@gmail.com>
2024-01-06 11:31:10 +0900

    feat: ノートの追加・削除

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
+import { useState } from 'react'
 import './App.css'
 import Main from './components/Main'
 import Sidebar from './components/Sidebar'
+import { v4 as uuid } from "uuid";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+  const onAddNote = () => {
+    const newNote = {
+      id: uuid(),
+      title: "新しいノート",
+      content: "新しいノートの内容",
+      modDate: Date.now(),
+    };
+    setNotes([...notes, newNote]);
+    console.log(notes);
+  }
+  const onDeleteNote = (id) => {
+    const filterNotes = notes.filter((note) => note.id !== id);
+    setNotes(filterNotes);
+  }
   return (
     <div className="App">
-      <Sidebar />
+      <Sidebar onAddNote={onAddNote}onDeleteNote={onDeleteNote} notes={notes}/>
       <Main />
     </div>
   )
